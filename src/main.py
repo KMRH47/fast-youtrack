facbcbb60984ab9e1df1f1e54b88ddf3d83a1085
Karsten <kmrh47@proton.me>
2024-09-30 21:51:53 +0200

    Display caught user errors

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
+from errors.user_error import UserError
 from requests import HTTPError
 from services.youtrack_service import YouTrackService
 from logger.my_logger import setup_logger
 from services.bearer_token_service import BearerTokenService
-from errors.invalid_passphrase_error import InvalidPassphraseError
-from errors.invalid_token_error import InvalidTokenError
 import sys
 import logging
 import traceback
@@ -15,6 +14,9 @@ logger = logging.getLogger(__name__)
 def main():
     passphrase = sys.argv[1] if len(sys.argv) > 1 else None
     subdomain = sys.argv[2] if len(sys.argv) > 2 else None
+
+    if (not passphrase) or (not subdomain):
+        raise ValueError("Passphrase and subdomain are required.")
 
     token_service = BearerTokenService(
         base_path=f"../user/{subdomain}",
@@ -33,7 +35,9 @@ if __name__ == "__main__":
     try:
         setup_logger()
         main()
-    except (InvalidPassphraseError, InvalidTokenError, HTTPError) as e:
+    except UserError as e:
+        e.display()
+    except HTTPError as e:
         logger.error(e.message)
     except Exception as e:
         logger.error(f'Unhandled exception\n{traceback.format_exc()}')
