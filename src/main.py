8efe1198bce188c5e11f185218937ebdd3e833bf
Karsten <kmrh47@proton.me>
2024-10-02 23:52:43 +0200

    Finish up story update ui
    Also add some random test in main

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
-from ui.youtrack_story_update_ui import display_youtrack_story_update
+from models.story_update import StoryState, StoryUpdate
+from utils.clipboard import get_selected_number
+from ui.youtrack_story_update_ui import display_update_youtrack_story_ui
 from errors.user_error import UserError
 from requests import HTTPError
 from services.youtrack_service import YouTrackService
@@ -10,6 +12,14 @@ import traceback
 
 
 logger = logging.getLogger(__name__)
+
+
+def update_youtrack_story_test(story_update: StoryUpdate) -> None:
+    try:
+        logger.info("Updating YouTrack story")
+        logger.info(f"Story update: {story_update}")
+    except Exception as e:
+        logger.error(f"Exception occurred: {e}")
 
 
 def main():
@@ -27,12 +37,13 @@ def main():
         subdomain=subdomain,
         bearer_token=token_service.get_bearer_token() or token_service.prompt_for_bearer_token())
 
-    #work_item_types =  youtrack_service.get_work_item_types()
-    #display_youtrack_story_update(available_states=[item.name for item in work_item_types])
-    test_list = ['testing', 'haha', 'okay']
-    display_youtrack_story_update(available_states=test_list)
+    work_item_types = youtrack_service.get_work_item_types()
+    story_update = StoryUpdate(
+        issue_id=f"AGI-{get_selected_number() or ''}",
+        state=StoryState(available_states=[item.name for item in work_item_types]))
 
-    # logger.info(f"Work Item Types (test): {work_item_types}")
+    display_update_youtrack_story_ui(story_update,
+                                     on_ok_clicked=lambda story_update: update_youtrack_story_test(story_update))
 
 
 if __name__ == "__main__":
--- src/ui/youtrack_story_update_ui.py
+++ src/ui/youtrack_story_update_ui.py
@@ -1,13 +1,17 @@
+import re
+from models.story_update import StoryState, StoryUpdate
+from tkinter import Entry, ttk
+from typing import Callable
+import time
 import logging
 import tkinter as tk
-from tkinter import ttk
-from typing import List
-import time
+
 
 logger = logging.getLogger(__name__)
 
 
-def display_youtrack_story_update(available_states: List[str]) -> None:
+def display_update_youtrack_story_ui(story_update: StoryUpdate,
+                                     on_ok_clicked: Callable[[StoryUpdate], None] = None) -> None:
     root = tk.Tk()
     root.title("Update YouTrack Story")
 
@@ -28,45 +32,82 @@ def display_youtrack_story_update(available_states: List[str]) -> None:
     def on_return(event=None):
         on_ok_click()
 
+    def get_available_states():
+        active_state = selected_state_var.get()
+        return [state for state in story_update.state.available_states if state != active_state]
+
+    def on_state_change(event):
+        state_combobox['values'] = get_available_states()
+
+    def delete_word(event, stopping_chars):
+        entry: tk.Entry = event.widget
+
+        cursor_pos = entry.index(tk.INSERT)
+
+        if not stopping_chars:
+            entry.delete(0, cursor_pos)
+            return "break"
+
+        pattern = f"[{''.join(map(re.escape, stopping_chars))}]"
+        text_up_to_cursor = entry.get()[:cursor_pos]
+
+        last_stop_index = -1
+        match = re.finditer(pattern, text_up_to_cursor)
+
+        for m in match:
+            if m.end() == cursor_pos:
+                continue
+            last_stop_index = m.start()
+
+        entry.delete(last_stop_index + 1, cursor_pos)
+
+        return "break"
+
     # Bind "Escape" key to close the window
     root.bind("<Escape>", on_escape)
     root.bind("<Return>", on_return)
 
     # Issue ID
-    tk.Label(root, text="Issue ID:").pack(anchor='w', padx=10, pady=(10, 0))
+    tk.Label(root, text="Issue ID:").pack(anchor='w', padx=10, pady=5)
     issue_id_entry = tk.Entry(root)
-    issue_id_entry.insert(0, "AGI-")  # Pre-fill with a default value if needed
-    issue_id_entry.pack(anchor='w', padx=10, pady=(0, 5),
-                        fill='x', expand=True)
+    issue_id_entry.insert(0, story_update.issue_id)
+    issue_id_entry.pack(anchor='w', padx=10, fill='x', expand=True)
     issue_id_entry.focus_force()
 
     # Enter Time
     tk.Label(root, text="Enter Time (e.g., 1h30m):").pack(anchor='w', padx=10)
     time_entry = tk.Entry(root)
+    time_entry.insert(0, story_update.time)
     time_entry.pack(anchor='w', padx=10, fill='x', expand=True)
 
     # Description
     tk.Label(root, text="Description:").pack(anchor='w', padx=10)
-    description_text = tk.Entry(root)
-    description_text.pack(anchor='w', padx=10,
-                          pady=(0, 5), fill='x', expand=True)
+    description_entry = tk.Entry(root)
+    description_entry.insert(0, story_update.description)
+    description_entry.pack(
+        anchor='w', padx=10,
+        pady=5, fill='x', expand=True)
 
     # Type
     tk.Label(root, text="Type:").pack(anchor='w', padx=10)
     type_entry = tk.Entry(root)
-    type_entry.pack(anchor='w', padx=10, pady=(0, 5), fill='x', expand=True)
+    type_entry.insert(0, story_update.type)
+    type_entry.pack(anchor='w', padx=10, pady=5, fill='x', expand=True)
 
     # State ComboBox
     tk.Label(root, text="Current State:").pack(
-        anchor='w', padx=10, pady=(10, 0))
-    state_combobox = ttk.Combobox(root, values=available_states)
-    state_combobox.set(available_states[0])
-    state_combobox.pack(anchor='w', padx=10, pady=(0, 5),
-                        fill='x', expand=True)
+        anchor='w', padx=10)
+    current_state = story_update.state.current or story_update.state.available_states[
+        0] or ""
+    selected_state_var = tk.StringVar(value=current_state)
+    state_combobox = ttk.Combobox(
+        root, values=get_available_states(), textvariable=selected_state_var)
+    state_combobox.pack(anchor='w', padx=10, pady=5, fill='x', expand=True)
+    state_combobox.bind('<<ComboboxSelected>>', on_state_change)
 
     # Elapsed Time
     elapsed_time_label = tk.Label(root, text="Elapsed Time: 00:00:00")
-    elapsed_time_label.pack(padx=10, pady=(10, 0), fill='x')
+    elapsed_time_label.pack(padx=10, fill='x', pady=5)
     elapsed_time_label.config(anchor='center')
 
     def update_elapsed_time():
@@ -78,19 +119,31 @@ def display_youtrack_story_update(available_states: List[str]) -> None:
         root.after(1000, update_elapsed_time)
 
     def on_ok_click():
-        log_message = (
-            f"YouTrack Story Update\n"
-            f"Issue ID: {issue_id_entry.get()}\n"
-            f"Time: {time_entry.get()}\n"
-            f"Description: {description_text.get()}\n"
-            f"Type: {type_entry.get()}\n"
-            f"State: {state_combobox.get()}"
-        )
-        logger.info(log_message)
+        logger.info("OK clicked")
+        if on_ok_clicked:
+            logger.info("Calling on_ok_clicked")
+            on_ok_clicked(StoryUpdate(
+                issue_id=issue_id_entry.get(),
+                time=time_entry.get(),
+                description=description_entry.get(),
+                type=type_entry.get(),
+                state=StoryState(
+                    current=selected_state_var.get(),
+                    available_states=get_available_states())
+            ))
         root.destroy()
 
     ok_button = tk.Button(root, text="OK", command=on_ok_click, width=10)
-    ok_button.pack(pady=(10, 10))
+    ok_button.pack(pady=5)
+
+    issue_id_entry.bind('<Control-BackSpace>',
+                        lambda event: delete_word(event, ['-']))
+    time_entry.bind('<Control-BackSpace>',
+                    lambda event: delete_word(event, ['d', 'h', 'm', 's']))
+    description_entry.bind('<Control-BackSpace>',
+                           lambda event: delete_word(event, []))
+    type_entry.bind('<Control-BackSpace>',
+                    lambda event: delete_word(event, []))
 
     update_elapsed_time()
     root.mainloop()
