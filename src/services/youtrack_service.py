65cf1801e919a9d3bc450d655153cc6a4878abd6
Karsten <kmrh47@proton.me>
2024-12-28 10:38:07 +0100

    Remove decorator entirely

--- src/services/youtrack_service.py
+++ src/services/youtrack_service.py
@@ -7,10 +7,8 @@ from models.general_responses import Issue, Project, StateBundleElement, User, W
 from constants.youtrack_queries import issue_query, bundle_query
 from models.general_requests import AddSpentTimeRequest
 from typing import TypeVar
-from common.decorators.cache_decorator import cached_response
 from stores.config_store import ConfigStore
 from common.storage.store import Store
-from common.decorators.base_cacheable import Cacheable
 
 
 logger = logging.getLogger(__name__)
@@ -18,18 +16,13 @@ logger = logging.getLogger(__name__)
 T = TypeVar("T")
 
 
-class YouTrackService(Cacheable):
+class YouTrackService:
     def __init__(
         self, http_client: HttpClient, store: Store, config_store: ConfigStore
     ):
         self._http_service = http_client
         self._store = store
-        self._config_store_instance = config_store
         self._request = http_client.request
-
-    @property
-    def _config_store(self) -> ConfigStore:
-        return self._config_store_instance
 
     def add_spent_time(
         self, issue_id: Optional[str], add_spent_time_request: AddSpentTimeRequest
@@ -46,29 +39,24 @@ class YouTrackService(Cacheable):
             logger.error(f"Could not add spent time to issue {issue_id}")
             raise e
 
-    @cached_response("user")
     def get_user_info(self) -> User:
         response = self._request(endpoint="users/me", fields="id,name,login,email")
         return User(**response)
 
-    @cached_response("issues")
     def get_issue(self, issue_id: str) -> Optional[Issue]:
         response = self._request(endpoint=f"issues/{issue_id}", fields=issue_query)
         return Issue(**response) if response else None
 
-    @cached_response("projects")
     def get_all_projects(self) -> List[Project]:
         response = self._request(endpoint="admin/projects", fields="id,name,shortName")
         return [Project(**item) for item in response]
 
-    @cached_response("workItemTypes")
     def get_work_item_types(self) -> List[WorkItem]:
         response = self._request(
             endpoint="admin/timeTrackingSettings/workItemTypes", fields="id,name"
         )
         return [WorkItem(**item) for item in response]
 
-    @cached_response("bundles")
     def get_bundle(self, bundle_id: str) -> List[StateBundleElement]:
         response = self._request(
             endpoint=f"admin/customFieldSettings/bundles/state/{bundle_id}/values",
